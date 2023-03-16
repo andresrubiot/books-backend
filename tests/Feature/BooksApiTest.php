33cc9ccafac53b058466f369cebf119615e7dde6
Andres Rubio <andresrubiot5@gmail.com>
2023-03-15 20:51:11 -0500

    Setup database and factory for can get all books test (index)

---
+++ tests/Feature/BooksApiTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Book;
+
+class BooksApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function can_get_all_books()
+    {
+        $books = Book::factory(4)->create();
+
+        $response = $this->getJson(route('books.index'));
+
+        foreach ($books as $book) {
+            $response->assertJsonFragment([
+                'title' => $book->title
+            ]);
+        }
+    }
+}
